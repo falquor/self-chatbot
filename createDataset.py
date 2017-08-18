8e2cad699cfcad6d348d310a8b8cc36f30586b19
falquor <cosinus221@gmail.com>
2017-08-19 01:37:27 +0200

    fonction de création du dataset terminée

--- createDataset.py
+++ createDataset.py
@@ -7,13 +7,13 @@ Created on Thu Aug 17 22:33:23 2017
 
 import pandas as pd
 import numpy as np
-import os
-import re
+#import os
+#import re
 from datetime import datetime
 
 def getFacebookData():
 	responseDictionary = dict()
-	fbFile = open('fbMessages.txt', 'r') 
+	fbFile = open('fbMessages.txt','utf8', 'r') 
 	allLines = fbFile.readlines()
 	myMessage, otherPersonsMessage, currentSpeaker = "","",""
 	for index,lines in enumerate(allLines):
@@ -46,20 +46,45 @@ def getFacebookData():
 	            otherPersonsMessage = justMessage[colon+2:] + otherPersonsMessage
 	        myMessage, otherPersonsMessage, currentSpeaker = "","",""    
 	return responseDictionary
- 
-fbFile = open('conv.txt','r')
-allLines = fbFile.readlines()
-#n = len(allLines)
-#for k in range(n):
-#    if k%3 == 1 :
-#        allLines.pop(k)
-name = "Quentin Bouniot"
-list_messages = []
-message = ''
-response = ''
-conversation = []
-n = len(allLines)
-k = 0
-for line in allLines:
-    if k%3 == 0:
-        
+
+def parseFbConv():
+    fbFile = open('conv.txt','r',encoding = 'utf8')
+    allLines = fbFile.readlines()
+    name = "Quentin Bouniot"
+    list_messages = []
+    message = ''
+    response = ''
+    space = ' '
+    conversation = ['','']
+    n = len(allLines)
+    my_message_last = False
+    for k in range(len(allLines)-3,-1,-1):
+#        if k%3 == 2:
+            if allLines[k][:15] != name and allLines[k][-7:-4] == 'UTC':
+                if not(my_message_last):
+                    if allLines[k+2][-7:-4] != 'UTC':
+                        message = message + space + allLines[k+2][:-1] + space
+                else:
+                    conversation[1] = response
+                    response = ''
+                    if conversation[0] != '':
+                        list_messages.append(conversation)
+                        conversation = ['','']
+                    message = allLines[k+2][:-1]
+                    my_message_last = False
+            if allLines[k][:15] == name and allLines[k][-7:-4] == 'UTC':
+                if my_message_last:
+                    if allLines[k+2][-7:-4] != 'UTC':
+                        response = response + space + allLines[k+2][:-1] + space
+                else:
+                    conversation[0] = message
+                    message = ''
+                    if conversation[1] != '':
+                        list_messages.append(conversation)
+                        conversation = ['','']
+                    response = allLines[k+2][:-1]
+                    my_message_last = True
+    
+    return list_messages
+
+list_messages = parseFbConv()
